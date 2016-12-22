bae4fe9fd36f129109aa6a11da85f431526fbfd4
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2016-12-22 11:55:27 +0530

    changed to high level consumer

--- kafkaconsumer.js
+++ kafkaconsumer.js
@@ -1,13 +1,21 @@
+var consumerId = Date.now();
+console.log('consumer Id:'+consumerId);
 var Promise = require('bluebird');
 var cassBl = require('./cass.js');
 var kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
+    HighLevelConsumer = kafka.HighLevelConsumer,
     client = new kafka.Client('172.24.36:2181,172.24.1.189:2181'),
-    consumer = new Consumer(
+    consumer = new HighLevelConsumer(
         client, [
             { topic: 'textmessages' }
-        ]
+        ],
+        {
+            groupId: 'nodeconsumer',
+            id:consumerId
+        }
     );
+
+
 
 
 consumer.on('message', function(message) {
@@ -16,7 +24,7 @@ consumer.on('message', function(message) {
     cassBl.put_in_cass(JSON.parse(message.value).messages)
         .then(function(result) {
 
-            console.log(result);
+            console.log('ConsumerId: '+consumerId+'   result: '+result);
         })
         .catch(function(err) {
             console.log('error occured while writing in cassandra:' + err)
