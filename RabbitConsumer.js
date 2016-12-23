13586f1e7443c52a51d198f9e8340bce36cf2298
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2016-12-23 15:40:00 +0530

    rabbitmq consumer added

---
+++ RabbitConsumer.js
@@ -0,0 +1,24 @@
+var amqp = require('amqplib/callback_api');
+var cassBl = require(__base + '/cass.js');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        var q = 'textmessages';
+        ch.assertQueue(q, { durable: true });
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+
+        ch.consume(q, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+
+            cassBl.put_in_cass(JSON.parse(msg.content.toString()).messages)
+                .then(function(result) {
+                    console.log(result);
+                })
+                .catch(function(err) {
+                    console.log('error occured while writing in cassandra:' + err)
+                })
+            ch.ack(msg);
+        }, { noAck: false });
+
+    });
+});
